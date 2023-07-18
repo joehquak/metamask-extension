8ab1b8abaa10886284fc666417df067ae46a4de0
Olusegun Akintayo <akintayo.segun@gmail.com>
2023-07-18 09:18:35 +0100

    lint fixes
    Signed-off-by: Olusegun Akintayo <akintayo.segun@gmail.com>

--- ui/components/app/gas-details-item/gas-details-item-title/gas-details-item-title.js
+++ ui/components/app/gas-details-item/gas-details-item-title/gas-details-item-title.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import { useI18nContext } from '../../../../hooks/useI18nContext';
-
 import Box from '../../../ui/box';
-<<<<<<< HEAD
-import InfoTooltip from '../../../ui/info-tooltip/info-tooltip';
-import { Text } from '../../../component-library/text/deprecated';
-=======
->>>>>>> de08fa152 (Add border and padding to gas component.)
 
 const GasDetailsItemTitle = () => {
   const t = useI18nContext();
